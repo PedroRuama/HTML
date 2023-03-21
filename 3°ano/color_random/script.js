2dada36e04d18c89a87093d8ebc138207838200b
USUÁRIO TEMPORÁRIO <aluno@bentoquirino.edu>
2023-03-21 15:52:16 -0300

    CorAleatoria

---
+++ 3°ano/color_random/script.js
@@ -0,0 +1,13 @@
+function corG(){
+    const cor = "0123456789ABCDEF"
+    const corG = '#'
+    for(let i = 0; i < 6; i++){
+        corG += Math.floor(Math.random( ) * cor.length)
+
+    }
+    return corG
+}
+
+function cor_add(){
+    document.getElementById("")
+}
